35144f6c2514626c72c9742e3d2df4454fc0077a
Collin Smith <collink33@gmail.com>
2015-08-07 15:40:59 -0700

    Update BoardShow - complete basic functionality

--- app/assets/javascripts/views/boards/board_show.js
+++ app/assets/javascripts/views/boards/board_show.js
@@ -2,15 +2,19 @@ TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
   template: JST['boards/board_show'],
 
   initialize: function () {
-    
+    this.model.lists().each(this.addListView.bind(this));
+    this.listenTo(this.model.lists(), 'add', this.addListView);
+    this.listenTo(this.model, 'sync', this.render);
   },
 
   render: function () {
-
+    this.html(this.template({ board: this.model }));
+    this.attachSubviews();
+    return this;
   },
 
-  addListView: function (listIndexView) {
-    var view = new TrelloClone.Views.ListShow();
+  addListView: function (list) {
+    var view = new TrelloClone.Views.ListShow({ model: list });
     this.addSubview('.lists', subview);
   }
 });
