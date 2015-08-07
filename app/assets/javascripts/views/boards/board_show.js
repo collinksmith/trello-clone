13bfffcbf0c2239e5defd6f93608c286e72325d9
Collin Smith <collink33@gmail.com>
2015-08-07 14:49:30 -0700

    Begin board_show view

---
+++ app/assets/javascripts/views/boards/board_show.js
@@ -0,0 +1,15 @@
+TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
+  template: JST['boards/board_show'],
+
+  initialize: function () {
+
+  },
+
+  render: function () {
+
+  },
+
+  addListIndexView: function (listIndexView) {
+    var view = new TrelloClone
+  }
+});
