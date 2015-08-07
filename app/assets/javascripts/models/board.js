d10e2bea6cab3e84593618b65281f54ba3e6e647
Collin Smith <collink33@gmail.com>
2015-08-07 15:29:09 -0700

    Add 'parse' and 'lists' functions to board model

--- app/assets/javascripts/models/board.js
+++ app/assets/javascripts/models/board.js
@@ -1,5 +1,20 @@
 TrelloClone.Models.Board = Backbone.Model.extend({
-  urlRoot: "/boards"
+  urlRoot: "/boards",
 
-  
+  parse: function (payload) {
+    if (payload.lists) {
+      this.lists().set(paylaod.lists);
+      delete payload.lists;
+    }
+
+    return payload
+  },
+
+  lists: function () {
+    if (this._lists === undefined) {
+      this._lists = new TrelloClone.Collections.Lists();
+    }
+
+    return this._lists;
+  }
 });
